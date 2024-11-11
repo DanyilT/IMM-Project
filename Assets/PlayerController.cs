fc0e21a907eb4ee43878bfa7f1205b06736f08a2
Artem Surzhenko <114075751+artemsa223@users.noreply.github.com>
2024-11-11 14:43:24 +0000

    Player controller
    simple player object +  wasd control

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+  public float speed = 5f;
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+}
