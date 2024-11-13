6618901decd6f0d78cdad6355da45a240707c6ac
Dany <71641218+DanyilT@users.noreply.github.com>
2024-11-13 23:48:29 +0000

    Update PlayerManager.cs

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,7 +6,6 @@ public class PlayerManager : MonoBehaviour
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private float spawnInterval = 1.0f;
     private float nextSpawnTime;
-    internal int playerCount = 1;
 
     // Update is called once per frame
     void Update()
@@ -22,25 +21,5 @@ public class PlayerManager : MonoBehaviour
     void SpawnProjectile()
     {
         Instantiate(projectilePrefab, transform.position, transform.rotation);
-    }
-
-    // Create copies of the player at the nearest available space on the ground
-    public void CreatePlayerCopies(int numberOfCopies)
-    {
-        for (int i = 0; i < numberOfCopies; i++)
-        {
-            Vector3 spawnPosition = FindNearestAvailableSpace();
-            Instantiate(gameObject, spawnPosition, transform.rotation);
-        }
-    }
-
-    // Find the nearest available space on the ground
-    private Vector3 FindNearestAvailableSpace()
-    {
-        Vector3 randomDirection = Random.insideUnitSphere * 5;
-        randomDirection += transform.position;
-        NavMeshHit hit;
-        NavMesh.SamplePosition(randomDirection, out hit, 5, 1);
-        return hit.position;
     }
 }
