1c8468e96c783859c0044faac772361149647994
Dany <71641218+DanyilT@users.noreply.github.com>
2024-11-13 23:48:07 +0000

    Update ObstacleManager.cs

--- Assets/Scripts/ObstacleManager.cs
+++ Assets/Scripts/ObstacleManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ObstacleManager : MonoBehaviour
 {
+    [SerializeField] private Material negativeObstacleCountMaterial;
+    [SerializeField] private Material positiveObstacleCountMaterial;
+    private Material zeroObstacleCountMaterial;
     [SerializeField] private int obstacleHitCount = 0;
     private TextMeshProUGUI value;
 
@@ -12,25 +15,72 @@ public class ObstacleManager : MonoBehaviour
     {
         // Get the Text on the Obstacle
         value = GetComponentInChildren<Canvas>().GetComponentInChildren<TextMeshProUGUI>();
+
+        // Take Default (Zero Count) Material
+        zeroObstacleCountMaterial = GetComponent<MeshRenderer>().material;
+
+        // Set the Text and Material based on the Obstacle Count
+        ObstacleHitCountWatcher();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Update the Text on the Obstacle
+        ObstacleHitCountWatcher();
+
+        // Destroy the Obstacle if the Obstacle behind the Player
+        DestroyIfBehindThePlayer();
+    }
+
+    // Update the Text and Material based on the Obstacle Count
+    private void ObstacleHitCountWatcher()
+    {
+        // Update the Text on the Obstacle
         value.text = obstacleHitCount.ToString();
+
+        // Set the Material based on the Obstacle Count
+        switch (obstacleHitCount)
+        {
+            case < 0:
+                GetComponent<MeshRenderer>().material = negativeObstacleCountMaterial;
+                break;
+            case > 0:
+                GetComponent<MeshRenderer>().material = positiveObstacleCountMaterial;
+                break;
+            default:
+                GetComponent<MeshRenderer>().material = zeroObstacleCountMaterial;
+                break;
+        }
+    }
+
+    // Destroy the Obstacle if the Obstacle behind the Player
+    private void DestroyIfBehindThePlayer()
+    {
+        if (transform.position.z < GameObject.Find("Player").GetComponent<Transform>().position.z)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void HandlePlayerCollision(Collision collision)
+    {
+        GameManager gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        gameManager.CreatePlayerCopies(obstacleHitCount);
+        Destroy(gameObject);
+    }
+
+    private void HandleProjectileCollision(Collision collision)
+    {
+        obstacleHitCount++;
     }
 
     // Handle collision
-    void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
-        // TODO: Idk why it is not working
-
         // Check if this gameObject collides with another GameObject
         if (collision.gameObject != null)
         {
-            Debug.Log($"Collision detected with: {collision.gameObject.name}");
-
             if (collision.gameObject.CompareTag("Player"))
             {
                 HandlePlayerCollision(collision);
@@ -40,19 +90,5 @@ public class ObstacleManager : MonoBehaviour
                 HandleProjectileCollision(collision);
             }
         }
-    }
-
-    void HandlePlayerCollision(Collision collision)
-    {
-        // TODO: Hope this works
-        PlayerManager playerManager = collision.gameObject.GetComponent<PlayerManager>();
-        playerManager.playerCount += obstacleHitCount;
-        playerManager.CreatePlayerCopies(playerManager.playerCount);
-        Destroy(gameObject);
-    }
-
-    void HandleProjectileCollision(Collision collision)
-    {
-        obstacleHitCount++;
     }
 }
