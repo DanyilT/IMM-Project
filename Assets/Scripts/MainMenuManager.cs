a2c65800400d8603700951dbf8e21d292fd825af
Dany <71641218+DanyilT@users.noreply.github.com>
2024-12-10 20:53:24 +0000

    Update MainMenuManager.cs (Now settings can do something, music)

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -8,28 +8,85 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] private GameObject settingsPanel;
     [SerializeField] private Button settingsButton;
     [SerializeField] private Button closeSettingsButton;
+    [SerializeField] private Slider backgroundMusicVolumeSlider;
+    [SerializeField] private Toggle backgroundMusicMuteToggle;
+    [SerializeField] private Slider sfxVolumeSlider;
+    [SerializeField] private Toggle sfxMuteToggle;
     [SerializeField] private string sceneName = "Main";
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startButton.onClick.AddListener(StartGame);
         settingsButton.onClick.AddListener(OpenSettings);
         closeSettingsButton.onClick.AddListener(CloseSettings);
+
+        backgroundMusicVolumeSlider.onValueChanged.AddListener(SetMusicVolume);
+        backgroundMusicMuteToggle.onValueChanged.AddListener(delegate { ToggleMute(backgroundMusicMuteToggle.isOn, "music"); });
+        sfxVolumeSlider.onValueChanged.AddListener(SetSFXVolume);
+        sfxMuteToggle.onValueChanged.AddListener(delegate { ToggleMute(sfxMuteToggle.isOn, "sfx"); });
+
+        backgroundMusicVolumeSlider.value = MusicManager.instance.MusicVolume;
+        backgroundMusicMuteToggle.isOn = MusicManager.instance.MusicVolume != 0;
+        sfxVolumeSlider.value = MusicManager.instance.SFXVolume;
+        sfxMuteToggle.isOn = MusicManager.instance.SFXVolume != 0;
     }
 
-    void StartGame()
+    private void StartGame()
     {
         SceneManager.LoadScene(sceneName);
     }
 
-    void OpenSettings()
+    private void OpenSettings()
     {
         settingsPanel.SetActive(true);
     }
 
-    void CloseSettings()
+    private void CloseSettings()
     {
         settingsPanel.SetActive(false);
+    }
+
+    private void SetMusicVolume(float volume)
+    {
+        MusicManager.instance.SetMusicVolume(volume);
+        if (volume > 0 && !backgroundMusicMuteToggle.isOn)
+        {
+            backgroundMusicMuteToggle.isOn = true;
+        }
+    }
+
+    private void SetSFXVolume(float volume)
+    {
+        MusicManager.instance.SetSFXVolume(volume);
+        if (volume > 0 && !sfxMuteToggle.isOn)
+        {
+            sfxMuteToggle.isOn = true;
+        }
+    }
+
+    private void ToggleMute(bool isUnmuted, string musicOrSFX)
+    {
+        if (musicOrSFX == "music")
+        {
+            if (isUnmuted)
+            {
+                MusicManager.instance.SetMusicVolume(backgroundMusicVolumeSlider.value);
+            }
+            else
+            {
+                MusicManager.instance.SetMusicVolume(0);
+            }
+        }
+        else if (musicOrSFX == "sfx")
+        {
+            if (isUnmuted)
+            {
+                MusicManager.instance.SetSFXVolume(sfxVolumeSlider.value);
+            }
+            else
+            {
+                MusicManager.instance.SetSFXVolume(0);
+            }
+        }
     }
 }
